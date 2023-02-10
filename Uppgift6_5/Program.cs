5d4aeec4e0e0878e607ab5d9458af80ce89c2bde
c <c>
2023-02-10 10:46:08 +0100

    Add project files.

---
+++ Uppgift6_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uppgift6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Längst("test12", "test12");
+            Längst("122", "121");
+        }
+        static void Längst(string ett, string två)
+        {
+            if (ett.Length > två.Length)
+            {
+                Console.WriteLine("Första är längst");
+                return;
+            }
+            else if (två.Length > ett.Length)
+            {
+                Console.WriteLine("Andra är längst");
+                return;
+            }
+            else if (ett.Length == två.Length) 
+            {
+                Console.WriteLine(ett);
+                return;
+            }
+        }
+    }
+}
